a1b3081efd8d87fd699c1e0115be0fdf74a022fc
iCAROS7 <hominlab@gmail.com>
2018-12-14 05:24:41 +0900

    Change Marker

--- resource/meetCenter.js
+++ resource/meetCenter.js
@@ -47,9 +47,9 @@ function meetCenter () {
         sumLat += Number((markersPos[i].newLat * 1).toFixed(4));
         sumLng += Number((markersPos[i].newLng * 1).toFixed(4));
     }
-    var imageSrc = 'http://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', // 마커이미지의 주소입니다
-        imageSize = new daum.maps.Size(64, 69), // 마커이미지의 크기입니다
-        imageOption = {offset: new daum.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+    var imageSrc = 'https://raw.githubusercontent.com/icaros7/meet-Center/master/resource/marker.png', // 마커이미지의 주소입니다
+        imageSize = new daum.maps.Size(96, 96), // 마커이미지의 크기입니다
+        imageOption = {offset: new daum.maps.Point(12, 0)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
     // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
     var markerImage = new daum.maps.MarkerImage(imageSrc, imageSize, imageOption);
